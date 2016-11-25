226e9cbb7730fc1e587bf932b8e4d906f84f3912
jim72zhou <jim72zhou@qq.com>
2016-11-25 11:34:06 +0800

    Create ITextPdfDemo.java

---
+++ ITextPdfDemo.java
@@ -0,0 +1,48 @@
+package pdf;
+
+import java.io.FileOutputStream;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+public class ITextPdfDemo {
+
+      public static void main(String[] args) throws Exception {
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("MyFirstJavaPdf.pdf"));
+        document.open();
+
+        Paragraph para = new Paragraph("Here is a table sample");
+        para.setSpacingAfter(10);
+        document.add(para);
+        
+        Image image = Image.getInstance("small1.jpg");
+        image.scalePercent(50);
+        image.setAlignment(Image.ALIGN_MIDDLE);
+        document.add(image);
+
+        PdfPTable table = new PdfPTable(3);
+        PdfPCell cell = new PdfPCell(new Paragraph("First Name"));
+
+        table.addCell(cell);
+        table.addCell("Last Name");
+        table.addCell("Gender");
+        table.addCell("Ram");
+        table.addCell("Kumar");
+        table.addCell("Male");
+        table.addCell("Lakshmi");
+        table.addCell("Devi");
+        table.addCell("Female");
+
+        document.add(table);
+        
+        image = Image.getInstance("big1.jpg");  
+        image.scalePercent(50);
+        document.add(image);
+
+        document.close();
+      }
+    }
