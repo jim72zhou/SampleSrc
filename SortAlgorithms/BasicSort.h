50483be2a42f81261b414a73c1286aa4d5980a97
jim72zhou <jim72zhou@qq.com>
2017-01-06 14:24:03 +0800

    Update quicksort and mergesort

---
+++ SortAlgorithms/BasicSort.h
@@ -0,0 +1,92 @@
+#ifndef SORT_BASIC_H
+#define SORT_BASIC_H
+
+#include <algorithm>
+
+
+/* Notes
+* 1.Because swap cost much time, selectionSort performs better than insertionSort.
+*	If changes swap to lower cost method2, insertionSort works better than selectionSort
+*/
+
+template<typename T>
+void selectionSort(T * pData, int length)
+{
+	for(int i = 0; i < length; ++i)
+	{
+		int minPos = i;
+		for(int j = i + 1; j < length; ++j)
+		{
+			if(pData[j] < pData[minPos])
+				minPos = j;
+		}
+		swap(pData[i], pData[minPos]);
+	}
+}
+
+template<typename T>
+void insertionSort(T * pData, int left, int right)
+{
+	for(int i = left + 1; i <= right; ++i)
+	{
+		T data = pData[i];
+		int j = 0;
+		for(j = i; j > left && data < pData[j - 1]; --j)
+		{
+			pData[j] = pData[j - 1];
+		}
+		pData[j] = data;
+	}
+}
+
+template<typename T>
+void insertionSort(T * pData, int length)
+{
+	insertionSort(pData, 0, length -1);	
+}
+
+template<typename T>
+void shellSort(T * pData, int length)
+{
+	int interval = 1;
+    while( interval < length / 3 )
+        interval = 3 * interval + 1;
+    //increment sequence: 1, 4, 13, 40, 121, 364, 1093...
+	
+	while(interval >= 1)
+	{
+		for(int i = interval; i < length; ++i)
+		{
+			T data = pData[i];
+			int j = 0;
+			for(j = i; j >= interval && data < pData[j - interval]; j -= interval)
+			{
+				pData[j] = pData[j - interval];
+			}
+			pData[j] = data;
+		}
+		interval /= 3;
+	}
+}
+
+template<typename T>
+void bubbleSort(T *pData, int length) 
+{
+	bool swapped = true;
+    int i = 0;
+
+    while (swapped) {
+		swapped = false;
+        ++i;
+        for (int j = 0; j < length - i; ++j) 
+		{
+            if (pData[j + 1] < pData[j]) 
+			{
+                swap(pData[j + 1], pData[j]);
+                swapped = true;
+            }
+		}
+	}
+}
+
+#endif //SORT_BASIC_H
---
+++ SortAlgorithms/QuickSort.h
@@ -0,0 +1,254 @@
+#ifndef SORT_QUICK_H
+#define SORT_QUICK_H
+
+#include <algorithm>
+#include <ctime>
+#include "BasicSort.h"
+
+// some topics about 3 ways quick sort
+// http://stackoverflow.com/questions/941447/quicksort-with-3-way-partition
+// http://www.geeksforgeeks.org/3-way-quicksort/
+// https://www.cs.princeton.edu/~rs/talks/QuicksortIsOptimal.pdf
+
+// 3 ways, optimized version
+template<typename T>
+void _quickSort(T pData[], int left, int right)
+{
+	const int INSERTION_SORT_THRESHOLD = 15;
+	if( right - left <= INSERTION_SORT_THRESHOLD )
+	{
+        insertionSort(pData, left, right);
+        return;
+    }
+
+    swap(pData[left] , pData[rand() % (right - left + 1) + left]);
+	T v = pData[left];
+
+    int i = left;
+	int j = right + 1;
+	int le = left;
+    int re = right + 1;
+
+	while(true)
+	{
+		// From left, find the first element greater than or equal to v. 
+		// This loop will definitely terminate as v is last element.
+		while(pData[++i] < v && i < j);
+
+		// From right, find the first element smaller than or equal to v
+		while(v < pData[--j] && i < j);
+
+		// If i and j cross, then we are done
+		if(i >= j)
+			break;
+
+		// Swap, so that smaller goes on left greater goes on right
+		swap(pData[i], pData[j]);
+
+		// Move all same left occurrence of pivot to beginning of array and keep count using lt
+		if(pData[i] == v)
+		{
+			++le;
+			swap(pData[le], pData[i]);
+		}
+
+		// Move all same right occurrence of pivot to end of array and keep count using gt
+		if(pData[j] == v)
+		{
+			--re;
+			swap(pData[re], pData[j]);
+		}
+	}
+
+	swap(pData[left], pData[i - 1]);
+
+	// Move all left same occurrences from beginning to adjacent to data[j]
+	j = i -1;
+	for(int k = left + 1; k <= le; ++k, --j)
+		swap(pData[k], pData[j]);
+
+	 // Move all right same occurrences from end to adjacent to data[i]
+	for(int k = right; k >= re; --k, ++i)
+		swap(pData[k], pData[i]);
+
+    _quickSort(pData, left, j);
+    _quickSort(pData, i, right);
+}
+
+// 3 ways, initial version
+template<typename T>
+void _quickSort3WaysV1(T * pData, int left, int right)
+{
+	const int INSERTION_SORT_THRESHOLD = 32;
+	if( right - left <= INSERTION_SORT_THRESHOLD )
+	{
+        insertionSort(pData, left, right);
+        return;
+    }
+
+    swap(pData[left] , pData[rand() % (right - left + 1) + left]);	
+	T v = pData[left];
+
+	int lt = left;			// data[left+1...lt] < v
+    int gt = right + 1;		// data[gt...right]	 > v
+    int i = left + 1;		// data[lt+1...i)   == v
+
+	while(i < gt)
+	{
+		if(pData[i] < v)
+		{
+            swap(pData[i], pData[lt + 1]);
+			++i;
+			++lt;
+        }
+		else if(v < pData[i])
+		{
+            swap(pData[i], pData[gt - 1]);
+			--gt;
+        }
+		else
+		{
+			++i;
+		}
+	}
+
+	swap(pData[left], pData[lt]);
+
+    _quickSort3WaysV1(pData, left, lt - 1);
+    _quickSort3WaysV1(pData, gt, right);
+}
+
+// 3 ways quick sort
+template<typename T>
+void quickSort(T * pData, int length)
+{
+	srand(time(NULL));
+	_quickSort3WaysV1(pData, 0, length - 1);
+}
+
+// execute partition for data[left...right] parts
+// return pilot
+// make data[left...pilot-1] < data[pilot], data[pilot+1...right] > data[pilot]
+template <typename T>
+int _partitionV2(T* pData, int left, int right)
+{
+	swap(pData[left] , pData[rand() % (right - left + 1) + left]);
+	
+	T v = pData[left];
+
+    // data[left+1...j] < v, data[j+1...i) > v
+	int i = left + 1;
+	int j = right;
+    while(true)
+	{
+        while(pData[i] < v && i <= j)
+		{
+            ++i;
+        }
+		while(v < pData[j] && i <= j)
+		{
+            --j;
+        }
+
+		if(i >= j)
+			break;
+
+		swap(pData[j], pData[i]);
+		++i;
+		--j;
+	}
+
+	swap(pData[left], pData[j]);
+
+    return j;
+}
+
+template <typename T>
+int _partitionV1(T* pData, int left, int right)
+{
+    // to avoid error for all data are greater than data[left] case
+	swap(pData[left] , pData[rand() % (right - left + 1) + left]);
+	
+	T v = pData[left];
+
+    int j = left; // data[left+1...j] < v, data[j+1...i) > v
+    for(int i = left + 1; i <= right; ++i)
+	{
+        if(pData[i] < v)
+		{
+            ++j;
+            swap(pData[j], pData[i]);
+        }
+	}
+
+	// to avoid error for all data are greater than data[left] case
+	// and if there are too much data, it would result in stack overflow
+    if(j > left)
+		swap(pData[left], pData[j]);
+
+    return j;
+}
+
+// improved version2
+template<typename T>
+void _quickSortV2(T * pData, int left, int right)
+{
+	const int INSERTION_SORT_THRESHOLD = 32;
+	if( right - left <= INSERTION_SORT_THRESHOLD )
+	{
+        insertionSort(pData, left, right);
+        return;
+    }
+
+    int pilot = _partitionV2(pData, left, right);
+    _quickSortV2(pData, left, pilot - 1);
+    _quickSortV2(pData, pilot + 1, right);
+}
+
+// improved version2
+template<typename T>
+void quickSortV2(T * pData, int length)
+{
+	srand(time(NULL));
+	_quickSortV2(pData, 0, length - 1);
+}
+
+// initial version
+template<typename T>
+void _quickSortV1(T * pData, int left, int right)
+{	
+	T pivot = pData[(left + right) / 2];
+
+	int i = left;
+	int j = right;
+
+	while(i < j)
+	{
+		while(pData[i] < pivot)
+			++i;
+		while(pData[j] > pivot)
+			--j;
+
+		if(i <= j)
+		{
+			swap(pData[i], pData[j]);
+			++i;
+			--j;
+		}
+	}
+
+	// recursion
+    if (left < j)
+		_quickSortV1(pData, left, j);
+    if (i < right)
+		_quickSortV1(pData, i, right);
+}
+
+// initial version
+template<typename T>
+void quickSortV1(T * pData, int length)
+{
+	_quickSortV1(pData, 0, length - 1);
+}
+
+#endif //SORT_QUICK_H
