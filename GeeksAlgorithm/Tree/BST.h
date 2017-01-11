8d51f3a17fc3215543558e91aabd223830864ad7
jim72zhou <jim72zhou@qq.com>
2017-01-11 14:41:19 +0800

    Add remove methods

--- GeeksAlgorithm/Tree/BST.h
+++ GeeksAlgorithm/Tree/BST.h
@@ -1,7 +1,8 @@
 ﻿#ifndef GEEKS_BST_H
 #define GEEKS_BST_H
 
-#include <algorithm>
+#include <queue>
+#include <cassert>
 
 using namespace std;
 
@@ -12,7 +13,7 @@ using namespace std;
 // https://en.wikipedia.org/wiki/Tree_(data_structure)#Terminology
 
 // Tree traversal: Pre-order, In-order, Post-order...
-// https://en.wikipedia.org/wiki/Pre-order_traversal
+// https://en.wikipedia.org/wiki/Tree_traversal
 
 template <typename Key, typename Value>
 class BST
@@ -31,6 +32,14 @@ private:
 			this->value = value;
 			this->left = this->right = nullptr;
 		}
+
+		TreeNode(TreeNode *node)
+		{
+			this->key = node->key;
+			this->value = node->value;
+			this->left = node->left;
+			this->right = node->right;
+		}
 	};
 
 public:
@@ -42,7 +51,7 @@ public:
 
 	~BST()
 	{
-		// TODO
+		destroy(m_root);
 	}
 
 	bool isEmpty()
@@ -70,6 +79,8 @@ public:
         return search(m_root, key);
     }
 
+	// Depth-first search: Pre-order, In-order, Post-order
+	// https://en.wikipedia.org/wiki/Tree_traversal
     void preOrder()
 	{
         preOrder(m_root);
@@ -85,23 +96,58 @@ public:
         postOrder(m_root);
     }
 
-    void printData(){        
-        cout<<"The tree size is: " << size() << endl;
-        cout<<"Root data in tree: ";
-		cout<< "root (" << m_root->key << "," << m_root->value << ")" <<  endl;
-		cout<< "left (" << m_root->left->key << "," << m_root->left->value << ")" <<  endl;
-		cout<< "right (" << m_root->right->key << "," << m_root->right->value << ")" <<  endl;
-		//int level = 0;
-        //for(int i = 0 ; i <= size() ; ++i)
+	// Breadth-first search: Level-order, In-order, Post-order
+    // https://en.wikipedia.org/wiki/Tree_traversal
+	void levelOrder()
+	{
+		if(m_root == nullptr)
+			return;
+
+		queue<TreeNode*> q;		
+		q.emplace(m_root);
+		while(!q.empty())
 		{
-   //         cout<<m_pData[i]<<" ";
-			//if(pow(2, level) == i)
-			//{
-			//	++level;
-			//	cout<<endl;
-			//}
+			TreeNode* node = q.front();
+			q.pop();
+
+			nodeAction(node);
+
+			if(node->left)
+				q.emplace(node->left);
+			if(node->right)
+				q.emplace(node->right);
 		}
-        cout<<endl;
+	}
+
+    Key minimum()
+	{
+        assert(m_root != nullptr);
+        TreeNode* minNode = minimum(m_root);
+        return minNode->key;
+    }
+
+    Key maximum()
+	{
+        assert(m_root != nullptr);
+        TreeNode* maxNode = maximum(m_root);
+        return maxNode->key;
+    }
+
+    void removeMin()
+	{
+        if(m_root)
+            m_root = removeMin(m_root);
+    }
+
+    void removeMax()
+	{
+        if(m_root)
+            m_root = removeMax(m_root);
+    }
+
+    void remove(Key key)
+	{
+        m_root = remove(m_root, key);
     }
 
 private:
@@ -177,11 +223,9 @@ private:
 	// just a placeholder for actual usage
 	void nodeAction(TreeNode * node)
 	{
-		cout<< "(" << node->key << "," << node->value << ")" <<  endl;
+		cout<< "(" << node->key << "," << node->value << ") ";
 	}
 
-	// Tree traversal: Pre-order, In-order, Post-order
-	// https://en.wikipedia.org/wiki/Pre-order_traversal
 	void preOrder(TreeNode * node)
 	{
         if(node == nullptr)
@@ -210,6 +254,134 @@ private:
 		postOrder(node->left);
 		postOrder(node->right);
 		nodeAction(node);
+    }
+
+    // implemented as iteratively 
+    TreeNode* minimum(TreeNode* node)
+	{
+		while(node->left)
+			node = node->left;
+
+		return node;
+    }
+
+	// implemented as recursively
+    TreeNode* maximum(TreeNode* node)
+	{
+        if(node->right == nullptr)
+			return node;
+
+		return maximum(node->right);
+    }
+
+	// implemented as recursively
+	// remove the min node from root node
+    // return new root after deleted min node
+    TreeNode* removeMin(TreeNode* node)
+	{
+		if(node->left == nullptr)
+		{
+			TreeNode * rightNode = node->right;
+			delete node;
+			--m_count;
+
+			return rightNode;
+		}
+
+		node->left = removeMin(node->left);
+		return node;
+    }
+
+	// implemented as iteratively
+	// remove the max node from root node
+    // return new root after deleted max node
+    TreeNode* removeMax(TreeNode* node)
+	{
+		TreeNode* rootNode = node;
+		TreeNode* parentNode = nullptr;
+		while(node->right)
+		{
+			parentNode = node;
+			node = node->right;
+		}
+
+		--m_count;
+		TreeNode* leftNode = node->left;
+		delete node;
+
+		if(parentNode)
+		{
+			parentNode->right = leftNode;
+			return rootNode;
+		}
+		else    // input node is root node
+		{
+			return leftNode;
+		}
+    }
+
+	TreeNode* remove(TreeNode* node, Key key)
+	{
+		if(node == nullptr)
+            return nullptr;
+
+        if(key < node->key)
+		{
+            node->left = remove(node->left, key);
+            return node;
+        }
+        else if(key > node->key)
+		{
+            node->right = remove(node->right, key);
+            return node;
+        }
+        else   // key == node->key
+		{
+			TreeNode *successor = nullptr;
+			if(node->left == nullptr)
+			{
+				successor = node->right;
+			}
+			else if(node->right == nullptr)
+			{
+				successor = node->left;
+			}
+			else   // node->left != nullptr && node->right != nullptr
+			 
+			// This page explains it very concisely and clearly
+			// http://quiz.geeksforgeeks.org/binary-search-tree-set-2-delete/
+
+			// Find inorder successor of the node, and copy contents of the 
+			// inorder successor to the node and delete the inorder successor. 
+			// In this particular case, inorder successor can be obtained by 
+			// finding the minimum value in right child of the node.
+
+			// Note that inorder predecessor can also be used.
+			{
+				successor = new TreeNode(minimum(node->right));
+				++m_count;
+
+				successor->right = removeMin(node->right);
+				successor->left = node->left;
+			}
+
+			delete node;
+            --m_count;
+
+            return successor;
+		}
+    }
+
+   // destroy the node as postOrder
+   void destroy(TreeNode * node)
+	{
+        if(node == nullptr)
+			return;
+
+		destroy(node->left);
+		destroy(node->right);
+		delete node;
+		--m_count;
     }
 
     TreeNode *m_root;
